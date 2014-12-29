b72cc142d7761053d8487705609b55e5ba904b1a
Andrei Ladyka <andrei@ladyka.tk>
2014-12-29 19:58:18 +0300

    q

--- Self-organizing_map_Kohonen/src/by/vurtatoo/Neiron.java
+++ Self-organizing_map_Kohonen/src/by/vurtatoo/Neiron.java
@@ -5,6 +5,7 @@
  */
 package by.vurtatoo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,41 +48,66 @@ public class Neiron {
     }
     
     public void learn(List<Letter> letters) {
-        List<Integer[]> a ;
-        int[][] a = new int[1][2];
+        List<Integer[]>  listVector = getVectors(letters);
+        /*int[][] a = new int[1][2];
         int[] b = new int[Stream.of(a).flatMap(Stream::of).collect(Collectors.toList()).size()][]
-        Stream.of(a).flatMap(Stream::of).collect(Collectors.toList()).toArray(b);
+        Stream.of(a).flatMap(Stream::of).collect(Collectors.toList()).toArray(b);*/
     }
-    public void Learn(List<Bitmap> ListBtm)
-		{
-			
-			
-			List<double[]> listVector = ListBtm.ConvertAll(x => x.GetVector()).ToList();
-			while(true)
-			{
-			var error = new List<double>();
-			
-			foreach(var source in listVector)
-			{
-				double[] y = calculate(source);
-				List<double> d = Enumerable.Range(0,OutputNeuronCount)
-					.Select(j => distanceEuclid(getW(j),source)*frec[j])
-					.ToList();
-				
-				int dwin = d.IndexOf(d.Min());
-				
-				for(int j = 0 ;j < InputNeuronCount; j++)
-				{
-					_weights[j,dwin] += betta*source[j] - _weights[j,dwin];
-				}
-				
-				
-				frec[dwin]++;
-				error.Add(distanceEuclid(getW(dwin), source));
-			}
-			
-			if(error.Max() < Accuracy) break;
-			}
+//    public void Learn(List<Bitmap> ListBtm)
+//    {
+//
+//
+//    List<double[]> listVector = ListBtm.ConvertAll(x => x.GetVector()).ToList();
+//    while(true)
+//    {
+//    var error = new List<double>();
+//
+//    foreach(var source in listVector)
+//    {
+//    double[] y = calculate(source);
+//    List<double> d = Enumerable.Range(0,OutputNeuronCount)
+//    .Select(j => distanceEuclid(getW(j),source)*frec[j])
+//    .ToList();
+//
+//    int dwin = d.IndexOf(d.Min());
+//
+//    for(int j = 0 ;j < InputNeuronCount; j++)
+//    {
+//    _weights[j,dwin] += betta*source[j] - _weights[j,dwin];
+//    }
+//
+//
+//    frec[dwin]++;
+//    error.Add(distanceEuclid(getW(dwin), source));
+//    }
+//
+//    if(error.Max() < Accuracy) break;
+//    }
+//
+//    }
 
-		}
+    private List<Integer[]> getVectors(List<Letter> letters) {
+        List<Integer[]> integers = new ArrayList<>();
+        
+        for (Letter letter : letters) {
+            for (Letter.ArrayLetterView a : letter.getArrayLetterViews()) {
+                //Integer[] vector = new Integer[25*25];
+                
+                Integer[] vector = convertArray(a.getArray());
+                
+            }
+        }
+        
+        
+        return integers;
+    }
+
+    private Integer[] convertArray(int[][] arr) {
+        Integer[] array;
+        array = new Integer[25*25];
+        
+        
+        
+        return array;
+    }
 }
